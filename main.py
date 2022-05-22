91c5f7362157f82fa304794fc40d9f20cd91ea00
Laszlo Simon <98slaci@gmail.com>
2022-05-22 19:38:09 +0200

    Game finished

--- main.py
+++ main.py
@@ -1,44 +1,66 @@
+import random
 import pygame
 from settings import *
 pygame.init()
+pygame.font.init()
 
 class Game():
     def __init__(self, width) -> None:
         self.w = width
         self.h = self.w
+        self.diff = 50
         self.title = pygame.display.set_caption("Tic Tac Toe")
         self.win = pygame.display.set_mode((self.w, self.h))
         self.game_over = False
         self.col = 3
         self.board = self.create_board(self.col)
-        self.board[1][1] = 'X'
-        self.board[1][0] = 'O'
+        self.player1 = 'X'
+        self.player2 = 'O'
+        self.current_player = self.player1 if random.random() < 0.5 else self.player2
+        self.font = pygame.font.SysFont("Arial", 16)
+        self.isFinished = False
     
     def create_board(self, col) -> list[list]:
         arr = list()
         for i in range(col):
             arr.append(list())
-            for j in range(col):
+            for _ in range(col):
                 arr[i].append("")
         return arr
     
     def draw_lines(self, win, col, width, height) -> None:
-        for i in range(col - 1):
-            pygame.draw.line(win, BLACK, ((i + 1) * width / col, 0), ((i + 1) * width / col, height))
-            pygame.draw.line(win, BLACK, (0, (i + 1) * height / col), (width, (i + 1) * height / col))
+        pass
+        pygame.draw.rect(win, GREY, (0, 0, width, self.diff), 0)
+        for i in range(col):
+            pygame.draw.line(win, BLACK, ((i + 1) * width / col, self.diff), ((i + 1) * width / col, height))
+            pygame.draw.line(win, BLACK, (0, i * (height - self.diff) / col + self.diff), (width, i * (height - self.diff) / col + self.diff))
     
     def get_mouse_index(self, pos, width, height, col) -> tuple[int, int]:
-        return (pos[0] // (width // col), pos[1] // (height // col))
+        return (pos[0] // (width // col), pos[1] // ((height + self.diff) // col))
 
     def draw_circle(self, win, pos, width, height, col) -> None:
-        pygame.draw.circle(win, BLACK, (pos[0] * (width // col) + (width // col) / 2, pos[1] * (height // col) + (height // col) / 2), (width // col - 2) / 2, 2)
+        pygame.draw.circle(win, BLACK, (pos[0] * (width / col) + (width / col) / 2, pos[1] * ((height - self.diff) / col) + ((height - self.diff)/ col) / 2 + self.diff), ((height - self.diff) / col - 5) / 2, 2)
 
     def draw_x(self, win, pos, width, height, col) -> None:
-        startX = pos[0] * (width / col)
-        startY = pos[1] * (height / col)
-        pygame.draw.line(win, BLACK, (startX + 5, startY + 5), (startX + (width / col) - 5, startY + (height / col) - 5), 2)
-        pygame.draw.line(win, BLACK, (startX + (width / col) - 5, startY + 5), (startX + 5, startY + (height / col) - 5), 2)
-        pass
+        pygame.draw.line(win, BLACK, (pos[0] * width / col + 5, pos[1] * (height - self.diff) / col + self.diff + 5), (pos[0] * width / col + width / col - 5, pos[1] * (height - self.diff) / col + (height - self.diff) / col + self.diff - 5), 2)
+        pygame.draw.line(win, BLACK, (pos[0] * width / col + 5, pos[1] * (height - self.diff) / col + (height - self.diff) / col + self.diff - 5), (pos[0] * width / col + width / col - 5, pos[1] * (height - self.diff) / col + self.diff + 5), 2)
+
+    def turn(self, pos) -> None:
+        x, y = pos
+        if not self.isFinished:
+            if self.board[x][y] == "":
+                self.board[x][y] = self.current_player
+                self.current_player = 'X' if self.current_player == 'O' else 'O'
+                self.isFinished = self.check_winner()
+        
+    def check_winner(self) -> bool:
+        for i, _ in enumerate(self.board):
+            if self.board[0][i] != '' and self.board[0][i] == self.board[1][i] and self.board[0][i] == self.board[2][i] or \
+               self.board[i][0] != '' and self.board[i][0] == self.board[i][1] and self.board[i][0] == self.board[i][2] or \
+               self.board[0][0] != '' and self.board[0][0] == self.board[1][1] and self.board[0][0] == self.board[2][2] or \
+               self.board[2][0] != '' and self.board[2][0] == self.board[1][1] and self.board[2][0] == self.board[0][2]:
+                return True
+        return False
 
     def draw(self, win) -> None:
         win.fill(WHITE)
@@ -50,6 +72,10 @@ class Game():
                     self.draw_circle(win, (i, j), self.w, self.h, self.col)
                 elif self.board[i][j] == 'X':
                     self.draw_x(win, (i, j), self.w, self.h, self.col)
+        if self.isFinished:
+            winner = self.player1 if self.current_player == self.player2 else self.player2
+            text = self.font.render(f"Winner is {winner}", 0, BLACK)
+            win.blit(text, (self.w - text.get_width() - 5, self.diff / 2 - text.get_height() / 2))
         pygame.display.update()
 
     def update(self) -> None:
@@ -59,7 +85,12 @@ class Game():
                     self.game_over = True
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     pos = self.get_mouse_index(pygame.mouse.get_pos(), self.w, self. h, self.col)
-                    self.board[pos[0]][pos[1]] = 'X'
+                    self.turn(pos)
+            
+            key = pygame.key.get_pressed()
+            if key[pygame.K_SPACE]:
+                self.board = self.create_board(self.col)
+                self.isFinished = False
             
             self.draw(self.win)
 
